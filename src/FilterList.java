289f4a9bbe824de3dbe6014812a6628907c9c2e7
Neha Agarwal <reachneha511@gmail.com>
2018-11-21 14:41:06 +0530

    Refectoring

---
+++ src/FilterList.java
@@ -0,0 +1,17 @@
+
+public class FilterList {
+
+
+    public void static filter(List<String> list){
+        
+    }
+    public static void main(String args[]){
+
+        List<String> list = Arrays.asList(new String []{"A", "B", "C", "D","E","F","G","H"});
+
+
+
+
+
+    }
+}
