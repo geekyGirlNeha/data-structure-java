8849395b56be138816a5453b3453729c20423a8f
Neha Agarwal <neha.agarwal@tothenew.com>
2019-02-10 21:59:36 +0530

    reversing sentence words

--- src/ReverseWords.java
+++ src/ReverseWords.java
@@ -1,3 +1,17 @@
+/*
+https://practice.geeksforgeeks.org/problems/reverse-words-in-a-given-string/0
+* Input:
+2
+i.like.this.program.very.much
+pqr.mno
+
+Output:
+much.very.program.this.like.i
+mno.pqr
+*
+*
+* */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,7 +27,8 @@ public class ReverseWords {
             String line=br.readLine();
             String [] arr = line.split("\\.");
             for(int j=arr.length-1;j!=-1;j--){
-                System.out.print(arr[j]+" ");
+                System.out.print(arr[j]);
+                if(j!=0){System.out.print(".");}
             }
 
         }
