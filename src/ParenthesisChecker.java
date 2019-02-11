7bd8a68b7452d5382901e226b3e3a96623cad5db
Neha Agarwal <neha.agarwal@tothenew.com>
2019-02-11 15:49:14 +0530

    parenthesis blance chancker

--- src/ParenthesisChecker.java
+++ src/ParenthesisChecker.java
@@ -1,73 +1,72 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
 public class ParenthesisChecker {
 
 
-    public static void isBalanced(String exp){
+    public static void isBalanced(String exp) {
 
         Stack<Character> stk = new Stack<>();
-        boolean flag=true;
+        boolean flag = true;
 
 
-        for(int i=0;i<exp.length();i++){
+        if (exp.length() == 1) {
+            System.out.println("Not balanced");
+            return;
+        }
+        for (int i = 0; i < exp.length(); i++) {
 
-            if(exp.charAt(i)=='(' || exp.charAt(i)=='[' || exp.charAt(i)=='{'){
+            if (exp.charAt(i) == '(' || exp.charAt(i) == '[' || exp.charAt(i) == '{') {
                 stk.push(exp.charAt(i));
-            }
-            else {
-                if (stk.empty()){
-                    flag=false;
+            } else {
+                if (stk.empty()) {
+                    flag = false;
                     break;
                 }
                 char top = stk.pop();
 
-                if(i==exp.length()-1 && !stk.empty()){
-                    flag=false;
+                if (i == exp.length() - 1 && !stk.empty()) {
+                    flag = false;
                     break;
                 }
 
-                if (exp.charAt(i)!=']'&& exp.charAt(i)!='}'&& exp.charAt(i)!=')'){
-                    flag=false;
+                if (exp.charAt(i) != ']' && exp.charAt(i) != '}' && exp.charAt(i) != ')') {
+                    flag = false;
                     break;
                 }
 
-                if (exp.charAt(i)==']'){
-                    if (top!='['){
-                        flag=false;
+                if (exp.charAt(i) == ']') {
+                    if (top != '[') {
+                        flag = false;
                         break;
                     }
-                } else if (exp.charAt(i)=='}'){
-                    if(top!='{'){
-                        flag=false;
+                } else if (exp.charAt(i) == '}') {
+                    if (top != '{') {
+                        flag = false;
                         break;
                     }
-                } else if (exp.charAt(i)==')'){
-                    if (top!='('){
-                        flag=false;
+                } else if (exp.charAt(i) == ')') {
+                    if (top != '(') {
+                        flag = false;
                         break;
                     }
                 }
             }
         }
 
-        if (flag){
+        if (flag) {
             System.out.println("Balanced");
-        }else {
+        } else {
             System.out.println("Not Balanced");
         }
     }
 
     public static void main(String[] args) throws IOException {
 
-        //to read input from console
-        /*BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String exp = br.readLine();
-        */
 
-        String exp="[()]{}{[()()]()}";
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String exp = br.readLine();
         isBalanced(exp);
+
     }
 }
