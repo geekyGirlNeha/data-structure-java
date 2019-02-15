78cba1e4bcf2d1b6080258b67ee2a001e6dce6f2
Neha Agarwal <neha.agarwal@tothenew.com>
2019-02-15 12:36:09 +0530

    subarray with given sum s

--- src/SubArrayWithGivenSum.java
+++ src/SubArrayWithGivenSum.java
@@ -6,32 +6,30 @@ import java.util.Scanner;
 
 public class SubArrayWithGivenSum {
 
-    public static void subArray(int [] arr,int S){
+    public static void subArray(int[] arr, int S) {
 
         outerloop:
-        for(int i=0;i<arr.length;i++){
+        for (int i = 0; i < arr.length; i++) {
 
-            int sum=0;
-            ArrayList<Integer> list=new ArrayList<>();
+            int sum = 0;
 
-            for(int j=i;j< arr.length;j++){
+            for (int j = i; j < arr.length; j++) {
 
-                sum=sum+arr[j];
-                list.add(arr[j]);
+                sum = sum + arr[j];
                 //sub array sum is equal to S
-                if(sum==S){
-                    int start=++i;
+                if (sum == S) {
+                    int start = ++i;
                     int end = ++j;
-                    System.out.println(i+" "+j);
+                    System.out.println(i + " " + j);
                     break outerloop;
 
                 }
 
-                if(i==arr.length-1&& sum!=S){
+                if (i == arr.length - 1 && sum != S) {
                     System.out.println(-1);
                 }
 
-                if(sum>S){
+                if (sum > S) {
                     break;
                 }
 
@@ -41,26 +39,24 @@ public class SubArrayWithGivenSum {
     }
 
 
-
-
     public static void main(String[] args) throws IOException {
 
         Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
+        int t = in.nextInt();
 
-        for (int i=0;i<t;i++){
+        for (int i = 0; i < t; i++) {
 
-            int n=in.nextInt();
-            int S=in.nextInt();
-            int [] arr=new int[n];
-            int j=0;
+            int n = in.nextInt();
+            int S = in.nextInt();
+            int[] arr = new int[n];
+            int j = 0;
 
-            while (j<n){
-                arr[j]=in.nextInt();
+            while (j < n) {
+                arr[j] = in.nextInt();
                 j++;
             }
 
-            subArray(arr,S);
+            subArray(arr, S);
 
         }
 
